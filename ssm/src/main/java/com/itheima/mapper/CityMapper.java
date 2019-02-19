181929e9b56ff51bf0eee95a32d60ae30b07335a
z_sir <zhangxingguo@itcast.cn>
2019-02-19 18:58:49 +0800

    1111

--- ssm/src/main/java/com/itheima/mapper/CityMapper.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.mapper;
-
-import com.itheima.domain.City;
-
-import java.util.List;
-
-public interface CityMapper {
-    List<City> findAll() throws Exception;
-}
--- ssm/src/main/java/com/itheima/service/CityService.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.service;
-
-import com.itheima.domain.City;
-
-import java.util.List;
-
-public interface CityService {
-    List<City> findAll() throws Exception;
-}
